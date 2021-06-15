4ff902e6e99e02cb48140836306980c111702bf2
Scorpion309 <Tustin@tut.by>
2021-06-15 20:27:44 +0300

    Add parser for command promt

---
+++ main.py
@@ -0,0 +1,17 @@
+import argparse
+
+
+def get_command():
+    parser = argparse.ArgumentParser(description='Convert long url to short')
+    parser.add_argument('url', type=str, help='Input url!')
+    parser.add_argument('--generate', action='store_const', const=True, default=False,
+                        help='Input "--generate" if you want to generate short url!')
+    parser.add_argument('--short_url', type=str, default=False,
+                        help='Input "--short_url" or if you want print and save short url!')
+    argsfromline = parser.parse_args()
+    return argsfromline
+
+
+if __name__ == '__main__':
+    args = get_command()
+    print(args)
