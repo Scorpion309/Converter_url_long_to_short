5b09689d18803ab9862a33059585e87527e5d4c8
Scorpion309 <Tustin@tut.by>
2021-06-21 20:42:14 +0300

    add links.db and query to create db and fix

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import argparse
-
+import sqlite3
 
 import shortuuid
+
+
+def new_db():
+    try:
+        sqlite_connection = sqlite3.connect('links.db')
+        cursor = sqlite_connection.cursor()
+        print('Connection to links.db successful')
+
+        sqlite_create_table_query = '''CREATE TABLE IF NOT EXISTS links (
+                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        url_id INTEGER,
+                                        FOREIGN KEY (url_id) REFERENCES urls(url_id));
+                                        '''
+        cursor.execute(sqlite_create_table_query)
+
+        sqlite_create_table_query = '''CREATE TABLE IF NOT EXISTS urls(
+                                        url_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        is_short BOOLEAN NOT NULL,
+                                        url VARCHAR(255) NOT NULL);'''
+        cursor.execute(sqlite_create_table_query)
+        cursor.close()
+    except sqlite3.Error as error:
+        print("Connection error!", error)
+    else:
+        sqlite_connection.commit()
+        print('Tables "links" and "urls" created successfully')
+    finally:
+        sqlite_connection.close()
+        print("Connection with links.db closed")
 
 
 def get_command():
@@ -21,8 +50,8 @@ def get_short_link(long_url):
     return short_link
 
 
-
 if __name__ == '__main__':
+    new_db()
     args = get_command()
     get_short_link(args.url)
     print(args)
